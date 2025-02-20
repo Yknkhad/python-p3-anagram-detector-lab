ff0c3f06a9313da1b0e88b9ba23568a9461ca5ed
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2025-02-20 23:32:52 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower() 
+        self.sorted_word = sorted(self.word) 
+
+    def match(self, word_list):
+        return [word for word in word_list if sorted(word.lower()) == self.sorted_word and word.lower() != self.word]
